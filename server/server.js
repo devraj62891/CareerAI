346a0c057c009ded8828826f1f70eb6eb7be31c7
devraj62891 <devraj62891@gmail.com>
2026-08-26 10:42:45 +0530

    server error fixed

--- server/server.js
+++ server/server.js
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on PORT:${PORT}`);
 });
