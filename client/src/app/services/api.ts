6a16af43ebd1d5468f9240e0f1468dc2f8fdf651
devraj62891 <devraj62891@gmail.com>
2026-09-04 12:17:51 +0530

    api fix

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -10,8 +10,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
  */
 
 // Change this if your backend runs on a different port/host.
-const API = 'http://localhost:3000/api'; 
-// const API = 'https://careerai-baceknd.onrender.com/api';
+// const API = 'http://localhost:3000/api'; 
+const API = 'https://careerai-baceknd.onrender.com/api';
 
 
 // ---- Shapes of the data the backend sends back ----
